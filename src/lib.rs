f1950e319203d758f737dd3b7c088e1bdece05f9
Ross Edwards <redwards64@hotmail.com>
2024-04-06 19:11:52 +0100

    test on windows vm

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
+#![allow(non_snake_case)]
+
 use std::ffi::c_void;
 
 use windows::{
     core::{self, PCWSTR},
     Win32::{
-        Foundation::{self, HINSTANCE, HMODULE},
+        Foundation::{self, HINSTANCE},
         Storage::FileSystem::{self},
         System::LibraryLoader,
         System::SystemServices::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH},
-        UI::{self},
+        UI,
     },
 };
 
@@ -15,7 +17,7 @@ const VV1: u32 = 3;
 const VV2: u32 = 8;
 const VV3: u32 = 1;
 
-const ADDR: u32 = 0xFEEF04BD;
+const DW_SIGNATURE: u32 = 0xFEEF04BD;
 
 macro_rules! hiword {
     ($l:expr) => {
@@ -25,30 +27,18 @@ macro_rules! hiword {
 
 macro_rules! loword {
     ($l:expr) => {
-        ($l >> 16) & 0xfff
+        ($l >> 16) & 0xffff
     };
 }
 
 #[no_mangle]
-#[allow(non_snake_case, unused_variables)]
-extern "system" fn DllMain(
-    dll_module: HINSTANCE,
-    call_reason: u32,
-    _: *mut (),
-) -> core::Result<()> {
+#[allow(unused_variables, improper_ctypes_definitions)]
+extern "system" fn DllMain(dll_module: HINSTANCE, call_reason: u32, _: *mut c_void) -> bool {
     match call_reason {
         DLL_PROCESS_ATTACH => {
-            if !check_version(dll_module) {
+            let correct_version = check_version(dll_module);
+            if correct_version {
                 unsafe {
-                    UI::WindowsAndMessaging::MessageBoxW(
-                        Foundation::HWND::default(),
-                        core::w!("The version of your game is not 3.8.1."),
-                        core::w!("Shit boy detected."),
-                        UI::WindowsAndMessaging::MB_OK | UI::WindowsAndMessaging::MB_ICONERROR,
-                    );
-                }
-            } else {
-		unsafe {
                     UI::WindowsAndMessaging::MessageBoxW(
                         Foundation::HWND::default(),
                         core::w!("The version of your game is 3.8.1."),
@@ -56,52 +46,60 @@ extern "system" fn DllMain(
                         UI::WindowsAndMessaging::MB_OK | UI::WindowsAndMessaging::MB_ICONERROR,
                     );
                 }
-	    }
+            }
+
+            correct_version
         }
         DLL_PROCESS_DETACH => detach(),
-        _ => (),
+        _ => false,
     }
-
-    Ok(())
 }
 
-fn check_version(_dll_module: HINSTANCE) -> bool {
-    let mut correct_version = true;
-
+fn check_version(dll_module: HINSTANCE) -> bool {
     unsafe {
-        let handle: Option<*mut u32> = Some(&mut 0);
-        let buf: &mut [u16] = &mut [];
-        LibraryLoader::GetModuleFileNameW(HMODULE::default(), buf);
-        let modname = PCWSTR::from_raw(buf.as_ptr());
-        let size = FileSystem::GetFileVersionInfoSizeW(modname, handle);
+        let mut mod_info = make_buffer();
+        let mod_info_buf = mod_info.as_mut_slice();
+        LibraryLoader::GetModuleFileNameW(dll_module, mod_info_buf);
 
-        if size == 0 {
-            let buf: *mut c_void = Vec::new().as_mut_ptr();
+        let mod_name = PCWSTR::from_raw(mod_info_buf.as_ptr());
+        let handle: Option<*mut u32> = Some(&mut u32::default());
+        let size = FileSystem::GetFileVersionInfoSizeW(mod_name, handle);
 
-            let _ = FileSystem::GetFileVersionInfoW(modname, u32::default(), size, buf);
+        if size != 0 {
+            let ver_info_buf = make_buffer().as_mut_ptr();
 
-            let info: *mut *mut c_void =
-                &mut (&mut FileSystem::VS_FIXEDFILEINFO::default() as *mut _ as *mut c_void);
+            let _ = FileSystem::GetFileVersionInfoW(mod_name, u32::default(), size, ver_info_buf);
 
-            let val: PCWSTR = core::w!("\\");
-            let len: *mut u32 = &mut 0;
+            let mut file_info =
+                &mut FileSystem::VS_FIXEDFILEINFO::default() as *mut _ as *mut c_void;
 
-            if FileSystem::VerQueryValueW(buf, val, info, len).as_bool() {
-                let info = &mut *(info as *mut FileSystem::VS_FIXEDFILEINFO);
+            if FileSystem::VerQueryValueW(
+                ver_info_buf,
+                core::w!(r"\"),
+                &mut file_info,
+                &mut u32::default(),
+            )
+            .as_bool()
+            {
+                let file_info = *(file_info as *mut FileSystem::VS_FIXEDFILEINFO);
 
-                if info.dwSignature == ADDR
-                    && hiword!(info.dwFileVersionMS) == VV1
-                    && loword!(info.dwFileVersionMS) == VV2
-                    && hiword!(info.dwFileVersionLS) == VV3
-                {
-                    correct_version = false;
-                };
-            };
-        };
+                file_info.dwSignature == DW_SIGNATURE
+                    && hiword!(file_info.dwFileVersionMS) == VV1
+                    && loword!(file_info.dwFileVersionMS) == VV2
+                    && hiword!(file_info.dwFileVersionLS) == VV3
+            } else {
+                panic!("Couldn't get version info from file!")
+            }
+        } else {
+            panic!("Error getting file info size!")
+        }
     }
-    correct_version
 }
 
-fn detach() {
-    ()
+fn detach() -> bool {
+    true
+}
+
+fn make_buffer<T>() -> Vec<T> {
+    Vec::<T>::with_capacity(Foundation::MAX_PATH as usize)
 }
